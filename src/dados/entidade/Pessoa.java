53a70831cd17c0fcd1f260ec2b1d131017bb145f
IFNMG <IFNMG@192.168.161.30>
2019-10-02 20:28:24 -0300

    Adicionei pacote META-INF e pacote UTIL

--- src/dados/entidade/Pessoa.java
+++ src/dados/entidade/Pessoa.java
@@ -1,7 +1,15 @@
 package dados.entidade;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public abstract class Pessoa {
     
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int codigo;
     private String nome;
     private String cpf;
