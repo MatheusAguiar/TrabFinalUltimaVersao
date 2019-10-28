6409b4fe520f096aa7276c221c4b48d4df8c63ef
mats- <mats-@PC-Matheus>
2019-10-28 12:08:12 -0300

    Todas as telas finalizadas

--- src/GUI/MenuController.java
+++ src/GUI/MenuController.java
@@ -5,12 +5,20 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -35,5 +43,122 @@ public class MenuController implements Initializable {
         data.setText(dataAtual);
         hora.setText(horaAtual);
     }    
+
+    @FXML
+    private void AbrirJanelaCadastroExemplar(ActionEvent event) throws IOException {
+        
+        Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/CadastrarExemplar.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Cadastro Exemplar"); 
+        s2.setScene(scene);
+        s2.show();
+    }
+
+    @FXML
+    private void AbrirJanelaCadastroUsuario(ActionEvent event) throws IOException {
+        
+         Stage s3 = new Stage();
+        s3.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/CadastrarUsuario.fxml"));
+        Scene scene = new Scene(root);
+        s3.resizableProperty().setValue(Boolean.FALSE);
+        s3.getIcons().add(new Image("/imagens/icone.jpg"));
+        s3.setTitle("Cadastro Usuário"); 
+        s3.setScene(scene);
+        s3.show();
+        
+    }
+
+    @FXML
+    private void AbrirJanelaCadastroLivro(ActionEvent event) throws IOException {
+        
+         Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/CadastroLivro.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Cadastro Livro"); 
+        s2.setScene(scene);
+        s2.show();
+    }
+
+    @FXML
+    private void AbrirJanelaConsultaLivro(ActionEvent event) throws IOException {
+        
+         Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/ConsultaLivro.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Consultar Livro"); 
+        s2.setScene(scene);
+        s2.show();
+        
+    }
+
+    @FXML
+    private void AbrirJanelaConsultaExemplar(ActionEvent event) throws IOException {
+        
+         Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/ConsultaExemplares.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Consultar Exemplar"); 
+        s2.setScene(scene);
+        s2.show();
+        
+    }
+
+    @FXML
+    private void AbrirJanelaConsultaUsuario(ActionEvent event) throws IOException {
+        
+         Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/ConsultaUsuario.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Consultar Usuário"); 
+        s2.setScene(scene);
+        s2.show();
+    }
+
+    @FXML
+    private void AbrirJanelaConsultaFunc(ActionEvent event) throws IOException {
+        
+         Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/ConsultaFuncionario.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Consultar Funcionário"); 
+        s2.setScene(scene);
+        s2.show();
+        
+    }
+
+    @FXML
+    private void AbrirJanelaEmprestimo(ActionEvent event) throws IOException {
+        
+         Stage s2 = new Stage();
+        s2.initModality(Modality.APPLICATION_MODAL);
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/JanelaEmprestimo.fxml"));
+        Scene scene = new Scene(root);
+        s2.resizableProperty().setValue(Boolean.FALSE);
+        s2.getIcons().add(new Image("/imagens/icone.jpg"));
+        s2.setTitle("Empréstimos"); 
+        s2.setScene(scene);
+        s2.show();
+        
+    }
     
 }
